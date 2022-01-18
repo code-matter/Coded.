f3ee9c60dc100e6962c3d91195285526ba3fc3e0
Alex Caissy <alexandre@04h11.com>
2022-01-18 12:22:35 -0500

    [FEATURE] - redid npm i

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./styles/App.scss";
 import HomePage from "./pages/HomePage";
-import {
-  AnimatePresence,
-  domAnimation,
-  LazyMotion,
-  motion,
-} from "framer-motion";
+import { AnimatePresence, domAnimation, LazyMotion } from "framer-motion";
 
 function App() {
   const location = useLocation();
