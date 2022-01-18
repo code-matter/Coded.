a9374575109452a79fdc4389af9e3fb65ec6c4cf
Alex Caissy <alexandre@04h11.com>
2022-01-18 11:43:55 -0500

    [FEATURE] - Added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./styles/App.scss";
 import HomePage from "./pages/HomePage";
+import {
+  AnimatePresence,
+  domAnimation,
+  LazyMotion,
+  motion,
+} from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence>
+          <Routes location={location} key={location.key}>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </AnimatePresence>
+      </LazyMotion>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { m } from "framer-motion";
+import React, { useState } from "react";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const HomePage = () => {
-  return <div>HOME PAGE</div>;
+  return (
+    <div className="homepage container">
+      <div className="homepage-content">
+        <m.h2 initial={{ x: "-20vw" }} animate={{ x: 0 }}>
+          CODE MATTER.
+        </m.h2>
+        <m.div
+          className="homepage-socials"
+          initial={{ x: "20vw" }}
+          animate={{ x: 0 }}
+        >
+          <a href="https://www.linkedin.com/in/alex-caissy-264ba71b1">
+            <FaLinkedinIn size={50} />
+          </a>
+          <a href="https://github.com/code-matter/code-matter">
+            <FaGithub size={50} />
+          </a>
+        </m.div>
+      </div>
+      <m.hr initial={{ width: 0 }} animate={{ width: "100%" }} />
+      <m.p initial={{ y: "20vh" }} animate={{ y: 0 }}>
+        CURRENTLY UNDER CONSTRUCTION
+      </m.p>
+    </div>
+  );
 };
 
 export default HomePage;
